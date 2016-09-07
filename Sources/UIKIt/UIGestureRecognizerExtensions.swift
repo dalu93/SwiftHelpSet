48ca91e75ee2766f333d9cbcaac9bf7a372adb3c
Luca D'Alberti <dalberti.luca93@gmail.com>
2016-09-07 17:28:48 +0200

    UIAlertController extensions + documentation

--- Sources/UIKIt/UIGestureRecognizerExtensions.swift
+++ Sources/UIKIt/UIGestureRecognizerExtensions.swift
@@ -33,6 +33,14 @@ extension UIGestureRecognizer {
 }
 
 extension UITapGestureRecognizer {
+    
+    /**
+     Creates and returns a gesture recognizer that performs a specific closure instead of a selector
+     
+     - parameter closure: The closure to execute
+     
+     - returns: The newly created UIGestureRecognizer subclass
+     */
     public static func recognizerPerforms(closure: () -> ()) -> UITapGestureRecognizer {
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(UIGestureRecognizer.trigger))
         recognizer.closureWrapper = ClosureWrapper(closure: closure)
@@ -41,6 +49,14 @@ extension UITapGestureRecognizer {
 }
 
 extension UIPinchGestureRecognizer {
+    
+    /**
+     Creates and returns a gesture recognizer that performs a specific closure instead of a selector
+     
+     - parameter closure: The closure to execute
+     
+     - returns: The newly created UIGestureRecognizer subclass
+     */
     public static func recognizerPerforms(closure: () -> ()) -> UIPinchGestureRecognizer {
         let recognizer = UIPinchGestureRecognizer(target: self, action: #selector(UIGestureRecognizer.trigger))
         recognizer.closureWrapper = ClosureWrapper(closure: closure)
@@ -49,6 +65,14 @@ extension UIPinchGestureRecognizer {
 }
 
 extension UIRotationGestureRecognizer {
+    
+    /**
+     Creates and returns a gesture recognizer that performs a specific closure instead of a selector
+     
+     - parameter closure: The closure to execute
+     
+     - returns: The newly created UIGestureRecognizer subclass
+     */
     public static func recognizerPerforms(closure: () -> ()) -> UIRotationGestureRecognizer {
         let recognizer = UIRotationGestureRecognizer(target: self, action: #selector(UIGestureRecognizer.trigger))
         recognizer.closureWrapper = ClosureWrapper(closure: closure)
@@ -57,6 +81,14 @@ extension UIRotationGestureRecognizer {
 }
 
 extension UISwipeGestureRecognizer {
+    
+    /**
+     Creates and returns a gesture recognizer that performs a specific closure instead of a selector
+     
+     - parameter closure: The closure to execute
+     
+     - returns: The newly created UIGestureRecognizer subclass
+     */
     public static func recognizerPerforms(closure: () -> ()) -> UISwipeGestureRecognizer {
         let recognizer = UISwipeGestureRecognizer(target: self, action: #selector(UIGestureRecognizer.trigger))
         recognizer.closureWrapper = ClosureWrapper(closure: closure)
@@ -65,6 +97,14 @@ extension UISwipeGestureRecognizer {
 }
 
 extension UIPanGestureRecognizer {
+    
+    /**
+     Creates and returns a gesture recognizer that performs a specific closure instead of a selector
+     
+     - parameter closure: The closure to execute
+     
+     - returns: The newly created UIGestureRecognizer subclass
+     */
     public static func recognizerPerforms<T>(closure: () -> ()) -> T {
         if let _ = T.self as? UIPanGestureRecognizer.Type {
             let recognizer = UIPanGestureRecognizer(target: self, action: #selector(UIGestureRecognizer.trigger))
@@ -81,6 +121,14 @@ extension UIPanGestureRecognizer {
 }
 
 extension UILongPressGestureRecognizer {
+    
+    /**
+     Creates and returns a gesture recognizer that performs a specific closure instead of a selector
+     
+     - parameter closure: The closure to execute
+     
+     - returns: The newly created UIGestureRecognizer subclass
+     */
     public static func recognizerPerforms(closure: () -> ()) -> UILongPressGestureRecognizer {
         let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(UIGestureRecognizer.trigger))
         recognizer.closureWrapper = ClosureWrapper(closure: closure)
